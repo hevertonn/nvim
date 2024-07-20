b73091b4f375105b2d61749444a3dad9e2a75150
Heverton <hb.hevertonborges@gmail.com>
2024-07-20 12:28:17 -0300

    change colorscheme to rose pine

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "rose-pine/nvim",
+    name = "rose-pine",
+    opts = {
+      styles = {
+        transparency = true,
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
